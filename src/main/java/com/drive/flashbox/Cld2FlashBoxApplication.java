b6c7f64aa0789da326e0bb1cc4905cd86a7d90f5
KangSeongKwan <kang53497@naver.com>
2025-02-18 15:47:50 +0900

    Initial Setting Final Project Team 5

---
+++ src/main/java/com/drive/flashbox/Cld2FlashBoxApplication.java
@@ -0,0 +1,13 @@
+package com.drive.flashbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cld2FlashBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cld2FlashBoxApplication.class, args);
+	}
+
+}
